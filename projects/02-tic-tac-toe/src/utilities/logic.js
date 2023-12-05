9ca32c799fa3983c66b8a790772c6ec6d59c9036
ShinyPotat <pachecomarquezjavier@gmail.com>
2023-12-05 13:39:35 +0100

    fix: check if square is null

--- projects/02-tic-tac-toe/src/utilities/logic.js
+++ projects/02-tic-tac-toe/src/utilities/logic.js
@@ -1,33 +1,32 @@
 export const checkWinner = (board) => {
+  const checkLine = (start, step) => {
+    const square1 = board[start];
+    const square2 = board[start + step];
+    const square3 = board[start + step * 2];
+
+    return square1 !== null && square1 === square2 && square2 === square3
+      ? square1
+      : null;
+  };
+
   // Check rows
-  for (let index = 0; index <= 6; index += 3) {
-    const square1 = board[index];
-    const square2 = board[index + 1];
-    const square3 = board[index + 2];
-    if (square1 === square2 && square2 === square3) {
-      return square1;
-    }
+  for (let index = 0; index < 9; index += 3) {
+    const winner = checkLine(index, 1);
+    if (winner) return winner;
   }
 
   // Check columns
   for (let index = 0; index < 3; index++) {
-    const square1 = board[index];
-    const square2 = board[index + 3];
-    const square3 = board[index + 6];
-    if (square1 === square2 && square2 === square3) {
-      return square1;
-    }
+    const winner = checkLine(index, 3);
+    if (winner) return winner;
   }
 
   // Check diagonal
-  for (let index = 0; index < 3; index += 2) {
-    const square1 = board[index];
-    const square2 = board[4];
-    const square3 = board[8 - index];
-    if (square1 === square2 && square2 === square3) {
-      return square1;
-    }
-  }
+  const diagonal1 = checkLine(0, 4);
+  if (diagonal1) return diagonal1;
+
+  const diagonal2 = checkLine(2, 2);
+  if (diagonal2) return diagonal2;
 
   return null;
 };
